8968078ace3378ce95b26066361d8b6c16080df4
Scoobert <marketing@MacBook-Pro-de-Tecnoinvwe.local>
2026-06-01 10:01:21 -0400

    feat: added a little addition calculator

---
+++ calculations.py
@@ -0,0 +1,10 @@
+def addition():
+
+  num1 = int(input("Add the first number to start the addition:\n"))
+  num2 = int(input("Add the second number and press enter to view the result:\n"))
+  addition_result = num1 + num2
+  
+  print(f"The result of the addition is = " + str(addition_result))
+
+
+addition()

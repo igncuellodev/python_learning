e9bdd7b3e762511360ec611adae7a62057d12a90
Scoobert <marketing@MacBook-Pro-de-Tecnoinvwe.local>
2026-06-01 17:49:37 -0400

    feat: I just made the first exercise, that consist on making the user to add 2 numbers and return the power result.

---
+++ mini_project_ex.py
@@ -0,0 +1,14 @@
+#Exercise 01
+
+base_number = int(input("Write the base, you want to power:\n"))
+exponent_number = int(input("Write the exponent and press enter to see the result:\n"))
+exponencial = base_number**exponent_number
+
+print("The result of the operation is = " + str(exponencial))
+
+
+#Excercise 02
+
+
+
+#Excercise 

5d2e98a1d844fcf5644ec111cfe2bd8dfe8e3f2b
igncuellodev <ignacio.cuello01@gmail.com>
2026-06-01 19:04:58 -0400

    feat: exercise 3 added

--- mini_project_ex.py
+++ mini_project_ex.py
@@ -1,14 +1,25 @@
 #Exercise 01
 
-base_number = int(input("Write the base, you want to power:\n"))
-exponent_number = int(input("Write the exponent and press enter to see the result:\n"))
-exponencial = base_number**exponent_number
-
-print("The result of the operation is = " + str(exponencial))
+#base_number = int(input("Write the base, you want to power:\n"))
+#exponent_number = int(input("Write the exponent and press enter to see the result:\n"))
+#exponencial = base_number**exponent_number
+#
+#print("The result of the operation is = " + str(exponencial))
 
 
 #Excercise 02
+import random
+user_number = int(input("Write a number to generate other 3 random numbers:\n"))
+num1 = random.randint(1,user_number)
+num2 = random.randint(1,user_number)
+num3 = random.randint(1,user_number)
+
+
+print(num1)
+print(num2)
+print(num3)
 
 
 
-#Excercise 
+#Excercise 03
+

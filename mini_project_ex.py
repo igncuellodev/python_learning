5da9698ba0217491050c3d1f825214f32ed913c2
igncuellodev <ignacio.cuello01@gmail.com>
2026-06-02 07:49:30 -0400

    feat: added the exercise file and miniproyecto01

--- mini_project_ex.py
+++ mini_project_ex.py
@@ -8,18 +8,34 @@
 
 
 #Excercise 02
-import random
-user_number = int(input("Write a number to generate other 3 random numbers:\n"))
-num1 = random.randint(1,user_number)
-num2 = random.randint(1,user_number)
-num3 = random.randint(1,user_number)
-
-
-print(num1)
-print(num2)
-print(num3)
+#import random
+#user_number = int(input("Write a number to generate other 3 random numbers:\n"))
+#num1 = random.randint(1,user_number)
+#num2 = random.randint(1,user_number)
+#num3 = random.randint(1,user_number)
+#
+#
+#print(num1)
+#print(num2)
+#print(num3)
 
 
 
 #Excercise 03
+
+#kid_01 = int(input("Write the age of the first kid:\n"))
+#kid_02 = int(input("Write the age of the second kid:\n"))
+#
+#if kid_01 < kid_02 and kid_02 - kid_01 >= 3:
+#    print("niño 02 es mucho mayor que niño 01")
+#
+#elif kid_01 < kid_02 and kid_02 - kid_01 < 3:
+#    print("niño 02 es algo mayor que niño 01")
+#
+#if kid_02 < kid_01 and kid_01 - kid_02 >= 3:
+#    print("niño 01 es mucho mayor que niño 02")
+#
+#else:
+#    print("niño 01 es algo mayor que niño 02")
+
 
---
+++ miniproyecto1.py
@@ -0,0 +1,3 @@
+#Debe generar un numero de 1 a 100 aleatorio y debe guardarse
+#El programa solicita el nombre del usuario y lo guarda
+#

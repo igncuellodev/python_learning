f3d949f86a07ae90ac54994b0d010c957e49d643
igncuellodev <ignacio.cuello01@gmail.com>
2026-06-08 21:48:18 -0400

    fix: made some minor adjustments to the miniproyecto1 code

--- mini_project_ex.py
+++ mini_project_ex.py
@@ -1,38 +1,38 @@
 
-base_number = int(input("Write the base, you want to power:\n"))
-exponent_number = int(input("Write the exponent and press enter to see the result:\n"))
-exponencial = base_number**exponent_number
-
-print("The result of the operation is = " + str(exponencial))
-import random
-user_number = int(input("Write a number to generate other 3 random numbers:\n"))
-num1 = random.randint(1,user_number)
-num2 = random.randint(1,user_number)
-num3 = random.randint(1,user_number)
-
-
-print(num1)
-print(num2)
-print(num3)
-
-
-
-
-kid_01 = int(input("Write the age of the first kid:\n"))
-kid_02 = int(input("Write the age of the second kid:\n"))
-
-if kid_01 < kid_02 and kid_02 - kid_01 >= 3:
-    print("niño 02 es mucho mayor que niño 01")
-
-elif kid_01 < kid_02 and kid_02 - kid_01 < 3:
-    print("niño 02 es algo mayor que niño 01")
-
-if kid_02 < kid_01 and kid_01 - kid_02 >= 3:
-    print("niño 01 es mucho mayor que niño 02")
-
-else:
-    print("niño 01 es algo mayor que niño 02")
-
+#base_number = int(input("Write the base, you want to power:\n"))
+#exponent_number = int(input("Write the exponent and press enter to see the result:\n"))
+#exponencial = base_number**exponent_number
+#
+#print("The result of the operation is = " + str(exponencial))
+#import random
+#user_number = int(input("Write a number to generate other 3 random numbers:\n"))
+#num1 = random.randint(1,user_number)
+#num2 = random.randint(1,user_number)
+#num3 = random.randint(1,user_number)
+#
+#
+#print(num1)
+#print(num2)
+#print(num3)
+#
+#
+#
+#
+#kid_01 = int(input("Write the age of the first kid:\n"))
+#kid_02 = int(input("Write the age of the second kid:\n"))
+#
+#if kid_01 < kid_02 and kid_02 - kid_01 >= 3:
+#    print("niño 02 es mucho mayor que niño 01")
+#
+#elif kid_01 < kid_02 and kid_02 - kid_01 < 3:
+#    print("niño 02 es algo mayor que niño 01")
+#
+#if kid_02 < kid_01 and kid_01 - kid_02 >= 3:
+#    print("niño 01 es mucho mayor que niño 02")
+#
+#else:
+#    print("niño 01 es algo mayor que niño 02")
+#
 
 
 
--- miniproyecto1.py
+++ miniproyecto1.py
@@ -1,8 +1,8 @@
 import random
 numero_random = random.randint(1,100)
-print(numero_random)
+#print(numero_random) #Este print lo hice para printear en consola el numero aleatorio y testear.
 nombre_user = input("Dinos tu nombre, antes de empezar:\n")
-user_num = int(input("Ingresa el numero que crees que es (0 para parar):\n"))
+user_num = int(input("Ingresa el número que crees que es (0 para parar):\n"))
 intentos = 0
 
 while user_num != numero_random:
@@ -16,62 +16,64 @@ while user_num != numero_random:
 
 
 
-        #Intervalo de 5
-        if user_num > numero_random and user_num - numero_random <= 5:
-            intentos = intentos + 1
+        #Aquí comienza el Intervalo de 5
+        if user_num > numero_random and user_num - numero_random <= 5: #En estas comparaciones estoy usando el ->and<- para que ambas condiciones se cumplan :)
+            intentos += 1 #Antes había colocado intento = intentos + 1 y en la ayudantía vimos el x += y que es lo mismo.
             print("Sorry " + nombre_user + ", ese no es pero estas a una distancia menor a 5")
-            user_num = int(input("Ingresa el numero que crees que es (0 para parar):\n"))
 
-        elif user_num < numero_random and numero_random - user_num <= 5:
-              intentos = intentos + 1
+
+        elif user_num < numero_random and numero_random - user_num <= 5:#En estas comparaciones estoy usando el ->and<- para que ambas condiciones se cumplan :)
+              intentos += 1 #Antes había colocado intento = intentos + 1 y en la ayudantía vimos el x += y que es lo mismo.
               print("Sorry " + nombre_user + ", ese no es pero estas a una distancia menor a 5")
-              user_num = int(input("Ingresa el numero que crees que es (0 para parar):\n"))
+      
 
-        #Intervalo de 5
+        #Aquí termina el Intervalo de 5
 
 
-        #Intervalo 10
-        elif user_num > numero_random and user_num - numero_random <= 10:
-              intentos = intentos + 1
+        #Aquí comienza el Intervalo 10
+        elif user_num > numero_random and user_num - numero_random <= 10: #En estas comparaciones estoy usando el ->and<- para que ambas condiciones se cumplan :)
+              intentos += 1 #Antes había colocado intento = intentos + 1 y en la ayudantía vimos el x += y que es lo mismo.
               print("Sorry " + nombre_user + ", ese no es pero estas a una distancia mayor a 5 y menor que 10")
-              user_num = int(input("Ingresa el numero que crees que es (0 para parar):\n"))
+      
 
 
-        elif user_num < numero_random and numero_random - user_num <= 10:
-              intentos = intentos + 1
+        elif user_num < numero_random and numero_random - user_num <= 10: #En estas comparaciones estoy usando el ->and<- para que ambas condiciones se cumplan :)
+              intentos += 1 #Antes había colocado intento = intentos + 1 y en la ayudantía vimos el x += y que es lo mismo.
               print("Sorry " + nombre_user + ", ese no es pero estas a una distancia mayor a 5 y menor que 10")
-              user_num = int(input("Ingresa el numero que crees que es (0 para parar):\n"))
-      #Intervalo 10
 
-      #Intervalo 20
+      #Aquí termina el Intervalo 10
 
-        elif user_num > numero_random and user_num - numero_random <= 20:
-            intentos = intentos + 1
+      #Aquí comienza el Intervalo 20
+
+        elif user_num > numero_random and user_num - numero_random <= 20: #En estas comparaciones estoy usando el ->and<- para que ambas condiciones se cumplan :)
+            intentos += 1 #Antes había colocado intento = intentos + 1 y en la ayudantía vimos el x += y que es lo mismo.
             print("Sorry " + nombre_user + ", ese no es pero estas a una distancia mayor a 10 y menor que 20")
-            user_num = int(input("Ingresa el numero que crees que es (0 para parar):\n"))
 
-        elif user_num < numero_random and numero_random - user_num <= 20:
-          intentos = intentos + 1
+
+        elif user_num < numero_random and numero_random - user_num <= 20: #En estas comparaciones estoy usando el ->and<- para que ambas condiciones se cumplan :)
+          intentos += 1 #Antes había colocado intento = intentos + 1 y en la ayudantía vimos el x += y que es lo mismo.
           print("Sorry " + nombre_user + ", ese no es pero estas a una distancia mayor a 10 y menor que 20")
-          user_num = int(input("Ingresa el numero que crees que es (0 para parar):\n"))
+          
 
-      #Intervalo 20
+      #Aquí termina el Intervalo 20
 
 
-      #Intervalo 20 o más
-        elif user_num > numero_random and user_num - numero_random >= 20:
-              intentos = intentos + 1
+      #Aquí comienza el Intervalo 20 o más
+        elif user_num > numero_random and user_num - numero_random >= 20: #En estas comparaciones estoy usando el ->and<- para que ambas condiciones se cumplan :)
+              intentos += 1 #Antes había colocado intento = intentos + 1 y en la ayudantía vimos el x += y que es lo mismo.
               print("Sorry " + nombre_user + ", ese no es pero estas a una distancia mayor a 20")
-              user_num = int(input("Ingresa el numero que crees que es (0 para parar):\n"))
-        elif user_num < numero_random and numero_random - user_num >= 20:
-              intentos = intentos + 1
+
+        elif user_num < numero_random and numero_random - user_num >= 20: #En estas comparaciones estoy usando el ->and<- para que ambas condiciones se cumplan :)
+              intentos += 1 #Antes había colocado intento = intentos + 1 y en la ayudantía vimos el x += y que es lo mismo.
               print("Sorry " + nombre_user + ", ese no es pero estas a una distancia mayor a 20")
-              user_num = int(input("Ingresa el numero que crees que es (0 para parar):\n"))
+      
+      
+        user_num = int(input("Ingresa el número que crees que es (0 para parar):\n")) #Al principio, había puesto esta variable en cada uno de los elifs, y el ayudante fernando, me recomendó que lo colocara al final de toda la cadena.
 
 
 
 
 
 if user_num == numero_random:
-       intentos = intentos + 1
+       intentos += 1
        print("Felicitaciones " + nombre_user + ", lo lograste en " + str(intentos) + " intentos")
